65c1cea24e342d196f072276595a0f7b1fc2d13e
Anti <maksudacse5737@gmail.com>
2024-02-27 18:09:03 +0600

    check button color change

--- src/C1.jsx
+++ src/C1.jsx
@@ -22,7 +22,7 @@ function C1({CtoP1,name,id,question1}) {
       <p>Answer is: <i  className="text-gray-500">{data}</i></p>
       <input className="p-5" type="text" placeholder='Answer promptly' onChange={(e)=>{setData(e.target.value)}}></input>
       <h5 className="text-purple-700 m-5">{CtoP1("Hi, I am the First Child")}</h5>
-      <button className="bg-yellow-500 m-5" onClick={check}>Check Answer</button>
+      <button className="bg-purple-600 m-5" onClick={check}>Check Answer</button>
     </div></div>
       )
   }
